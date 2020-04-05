61dcc7de40ff4edcfe22be94f6e915488a5080d4
Hugo <hpeters1024.nl>
2020-04-05 19:57:41 +0200

    Hardcoded point light

--- rendering/rendering.h
+++ rendering/rendering.h
@@ -86,66 +86,15 @@ void Rasterizer::loop(const Camera::Camera& camera) {
 
   mat4x4 cam_matrix;
   camera.getMatrix().unpack(cam_matrix);
+
+  Vector3 camPos = camera.getPosition();
   glUniformMatrix4fv(SH_UNIFORM_CAMERA, 1, GL_FALSE, (const GLfloat*)(cam_matrix));
+  glUniform3f(SH_UNIFORM_CAMERA_POS, camPos.x, camPos.y, camPos.z);
+  glUniform1f(SH_UNIFORM_TIME, glfwGetTime());
 
   for (auto &obj : m_scene) {
     obj->draw();
   }
-  glfwSwapBuffers(m_window);
-  glfwPollEvents();
-}
-
-class Raytracer : public GLFWRenderer
-{
-private:
-    Logger m_logger = Logger("Raytracer");
-    GLuint m_shader;
-    std::shared_ptr<MeshRepo> meshRepo;
-    std::vector<ISceneObject*> m_scene;
-public:
-    Raytracer();
-    void loop(const Camera::Camera& camera) override;
-    inline GLFWwindow* getWindowPointer() const override { return m_window; }
-    inline void addCube(rp3d::ProxyShape* shape, rp3d::BoxShape* box) override { m_scene.push_back(new CubeObject(meshRepo.get(), shape, box)); }
-};
-
-Raytracer::Raytracer() {
-  if (!glfwInit())
-  {
-    m_logger.logError("Could not initialize GLFW");
-    exit(-2);
-  }
-
-  glfwSetErrorCallback(error_callback);
-  glEnable(GL_DEBUG_OUTPUT);
-  glDebugMessageCallback(GLDEBUGPROC(gl_debug_output), nullptr);
-  glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-  m_window = glfwCreateWindow(604, 480, "Physics", NULL, NULL);
-  if (!m_window){
-    m_logger.logError("Could not create window.");
-    exit(-3);
-  }
-  glfwMakeContextCurrent(m_window);
-  glfwSwapInterval(1);
-
-  meshRepo = std::make_shared<MeshRepo>();
-  auto vs = CompileShader(GL_VERTEX_SHADER, shaders::vs_src);
-  auto fs = CompileShader(GL_FRAGMENT_SHADER, shaders::fs_src);
-  m_shader = GenerateProgram(vs, fs);
-
-  m_logger.logDebug("Initialized");
-}
-
-void Raytracer::loop(const Camera::Camera& camera) {
-  glfwGetFramebufferSize(m_window, &m_window_width, &m_window_height);
-  glViewport(0, 0, m_window_width, m_window_height);
-
-  glUseProgram(m_shader);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
   glfwSwapBuffers(m_window);
   glfwPollEvents();
 }
--- rendering/shaders.h
+++ rendering/shaders.h
@@ -3,6 +3,7 @@
 #define SH_UNIFORM_CAMERA 0
 #define SH_UNIFORM_MVP 1
 #define SH_UNIFORM_CAMERA_POS 2
+#define SH_UNIFORM_TIME 3
 
 namespace shaders
 {
@@ -14,28 +15,47 @@ namespace shaders
 
       layout(location = 0) uniform mat4 uCamera;
       layout(location = 1) uniform mat4 uMvp;
-      layout(location = 2) uniform vec3 uCamPos;
 
-      out vec3 color;
+      out vec3 fragPos;
+      out vec3 fragNormal;
 
       void main()
       {
         vec4 worldPos = uMvp * vec4(vPos, 1.0);
-        vec3 worldNormal = (uMvp * vec4(vNormal,0)).xyz;
+        fragPos = worldPos.xyz;
+        fragNormal = normalize((uMvp * vec4(vNormal,0)).xyz);
         gl_Position = uCamera * worldPos;
-        color = (max(dot(normalize(vec3(1,1,0)), worldNormal) ,0)* 0.6 + 0.4) * vec3(1);
       }
     )";
 
     static const char* fs_src = R"(
       #version 450
 
-      in vec3 color;
-      out vec3 o_color;
+      layout(location = 2) uniform vec3 uCamPos;
+      layout(location = 3) uniform float uTime;
+
+      in vec3 fragPos;
+      in vec3 fragNormal;
+      out vec3 color;
 
       void main()
       {
-          o_color = color;
+          vec3 materialColor = vec3(1);
+          vec3 lightPos = vec3(10*sin(uTime), 10, 10*cos(uTime));
+          vec3 lightCol = vec3(1, 0.8, 0.4) * 100;
+
+          vec3 lightRay = lightPos - fragPos;
+          float lightDis2 = dot(lightRay, lightRay);
+          float falloff = 1.0f / lightDis2;
+          vec3 lightNormal = lightRay / sqrt(lightDis2);
+          float diffuse = max(dot(lightNormal, fragNormal), 0);
+
+          vec3 eye = normalize(fragPos - uCamPos);
+          vec3 refl = normalize(reflect(eye, fragNormal));
+          float spec = pow(max(dot(refl, lightNormal), 0), 30) * 0.75;
+
+          float ambient = 0.1f;
+          color = ambient * materialColor + (diffuse + spec) * materialColor * lightCol * falloff;
       }
     )";
 }
