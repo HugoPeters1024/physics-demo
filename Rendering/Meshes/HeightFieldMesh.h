945d357e0951d1d784d566f9efc0cc55f1e597ea
Hugo <hpeters1024.nl>
2020-04-07 19:07:18 +0200

    Use element buffer for heightfield map

--- Rendering/Meshes/HeightFieldMesh.h
+++ Rendering/Meshes/HeightFieldMesh.h
@@ -3,6 +3,7 @@ class HeightFieldMesh
 private:
     GLuint m_vao;
     GLuint m_vbo;
+    GLuint m_veb;
     GLuint m_vertex_count;
     const DefaultShader* m_shader;
     Material m_material;
@@ -24,8 +25,8 @@ public:
       glGenVertexArrays(1, &m_vao);
       glBindVertexArray(m_vao);
 
-      std::unordered_map<int, std::vector<Vector3>> normalAcc;
-      std::unordered_map<int, Vector3> vertices;
+      std::vector<Vector3> normalAcc[width * height];
+      Vector3 vertices[width * height];
       for(int y=0; y<height-1; y++) {
         for (int x = 0; x < width - 1; x++) {
           int x0 = x, y0 = y;
@@ -61,84 +62,61 @@ public:
       }
 
       std::vector<float> vbo_data;
-      for(int y=0; y<height-1; y++) {
-        for (int x = 0; x < width - 1; x++) {
-          int x3 = x, y3 = y;
-          int x2 = x + 1, y2 = y;
-          int x1 = x + 1, y1 = y + 1;
-          int x0 = x, y0 = y + 1;
+      for(int y=0; y<height; y++) {
+        for (int x = 0; x < width; x++) {
 
-          auto v0 = vertices[x0 + y0 * width];
-          auto v1 = vertices[x1 + y1 * width];
-          auto v2 = vertices[x2 + y2 * width];
-          auto v3 = vertices[x3 + y3 * width];
+          auto v = vertices[x + y * width];
+          Vector3 n = normalAverage(normalAcc[x + y * width]);
 
-          Vector3 n0 = normalAverage(normalAcc[x0 + y0 * width]);
-          Vector3 n1 = normalAverage(normalAcc[x1 + y1 * width]);
-          Vector3 n2 = normalAverage(normalAcc[x2 + y2 * width]);
-          Vector3 n3 = normalAverage(normalAcc[x3 + y3 * width]);
-
-          vbo_data.push_back(v0.x);
-          vbo_data.push_back(v0.y);
-          vbo_data.push_back(v0.z);
-          vbo_data.push_back(n0.x);
-          vbo_data.push_back(n0.y);
-          vbo_data.push_back(n0.z);
-
-          vbo_data.push_back(v1.x);
-          vbo_data.push_back(v1.y);
-          vbo_data.push_back(v1.z);
-          vbo_data.push_back(n1.x);
-          vbo_data.push_back(n1.y);
-          vbo_data.push_back(n1.z);
-
-          vbo_data.push_back(v3.x);
-          vbo_data.push_back(v3.y);
-          vbo_data.push_back(v3.z);
-          vbo_data.push_back(n3.x);
-          vbo_data.push_back(n3.y);
-          vbo_data.push_back(n3.z);
-
-          vbo_data.push_back(v1.x);
-          vbo_data.push_back(v1.y);
-          vbo_data.push_back(v1.z);
-          vbo_data.push_back(n1.x);
-          vbo_data.push_back(n1.y);
-          vbo_data.push_back(n1.z);
-
-          vbo_data.push_back(v2.x);
-          vbo_data.push_back(v2.y);
-          vbo_data.push_back(v2.z);
-          vbo_data.push_back(n2.x);
-          vbo_data.push_back(n2.y);
-          vbo_data.push_back(n2.z);
-
-          vbo_data.push_back(v3.x);
-          vbo_data.push_back(v3.y);
-          vbo_data.push_back(v3.z);
-          vbo_data.push_back(n3.x);
-          vbo_data.push_back(n3.y);
-          vbo_data.push_back(n3.z);
+          vbo_data.push_back(v.x);
+          vbo_data.push_back(v.y);
+          vbo_data.push_back(v.z);
+          vbo_data.push_back(n.x);
+          vbo_data.push_back(n.y);
+          vbo_data.push_back(n.z);
         }
       }
 
-      m_vertex_count = vbo_data.size() / 6;
+      std::vector<uint> indices;
+      for(int y=0; y<height-1; y++)
+      {
+        for(int x=0; x<width-1; x++) {
+          int p0 = x + y * width;
+          int p1 = (x+1) + y * width;
+          int p2 = (x+1) + (y+1) * width;
+          int p3 = x + (y+1) * width;
+
+          indices.push_back(p2);
+          indices.push_back(p1);
+          indices.push_back(p0);
+
+          indices.push_back(p0);
+          indices.push_back(p3);
+          indices.push_back(p2);
+        }
+      }
+      m_vertex_count = indices.size();
+
+      glGenBuffers(1, &m_veb);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_veb);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint), indices.data(), GL_STATIC_DRAW);
 
       glGenBuffers(1, &m_vbo);
       glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-      glBufferData(GL_ARRAY_BUFFER, width * height * 6 * 6 * sizeof(float), vbo_data.data(), GL_STATIC_DRAW);
+      glBufferData(GL_ARRAY_BUFFER, vbo_data.size() * sizeof(float), vbo_data.data(), GL_STATIC_DRAW);
 
       glEnableVertexAttribArray(DefaultShader::SH_IN_VPOS);
       glEnableVertexAttribArray(DefaultShader::SH_IN_VNORMAL);
       glVertexAttribPointer(DefaultShader::SH_IN_VPOS, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(sizeof(float) * 0));
       glVertexAttribPointer(DefaultShader::SH_IN_VNORMAL, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(sizeof(float) * 3));
+      glVertexArrayElementBuffer(m_vao, m_veb);
       glBindVertexArray(0);
 
     }
     void draw(const Camera::Camera* camera, const Matrix4& mvp) const {
       glBindVertexArray(m_vao);
       m_shader->use(camera, mvp, m_material);
-      glDrawArrays(GL_TRIANGLES, 0, m_vertex_count);
+      glDrawElements(GL_TRIANGLES, m_vertex_count, GL_UNSIGNED_INT, (void*)nullptr);
       glBindVertexArray(0);
     }
 };
